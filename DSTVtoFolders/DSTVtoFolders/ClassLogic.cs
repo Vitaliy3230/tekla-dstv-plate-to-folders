f9a00706f2f6aa34efc055e0978c782888add137
Vitaliy3230 <44684547+Vitaliy3230@users.noreply.github.com>
2019-11-09 22:10:23 +0200

    Add files via upload

---
+++ DSTVtoFolders/DSTVtoFolders/ClassLogic.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+
+namespace DSTVtoFolders
+{
+    public class MyPlate
+    {
+        public string PlateProfile { get; set; }
+        public string PlateMaterial { get; set; }
+
+        public void Add(string PlateProfile, string PlateMaterial)
+        {
+            this.PlateProfile = PlateProfile;
+            this.PlateMaterial = PlateMaterial;
+        }
+
+    }
+
+    public class MyPlateComparer : IEqualityComparer<MyPlate>
+    {
+        public bool Equals(MyPlate x, MyPlate y)
+        {
+            return x.PlateMaterial == y.PlateMaterial
+                   &&
+                   x.PlateProfile == y.PlateProfile;
+        }
+
+        public int GetHashCode(MyPlate mp)
+        {
+            unchecked
+            {
+                return mp.PlateMaterial.GetHashCode() ^
+                       mp.PlateProfile.GetHashCode();
+            }
+        }
+    }
+}
---
+++ DSTVtoFolders/DSTVtoFolders/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace DSTVtoFolders
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.button1.Location = new System.Drawing.Point(0, 74);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(161, 51);
+            this.button1.TabIndex = 0;
+            this.button1.Tag = "";
+            this.button1.Text = "Выбрать и Создать";
+            this.toolTip1.SetToolTip(this.button1, "Детали должны быть пронумерованы\r\nПосле нажатия выберите детали в Tekla и нажмите" +
+        " колесико миши.");
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(13, 13);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(102, 20);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "PL";
+            this.toolTip1.SetToolTip(this.textBox1, "Префикс профиля пластин");
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(13, 40);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 2;
+            this.textBox2.Text = "CNC_Plate";
+            this.toolTip1.SetToolTip(this.textBox2, "Название папки хранения DSTV");
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(161, 125);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.Name = "Form1";
+            this.Text = "Генератор сортировки dstv пластин по папкам";
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
